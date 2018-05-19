091d2a5c75f4054dbbf36da3d16c32e771172dbb
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-05-19 18:45:34 -0500

    Created the PopularMoviesSettings class to hold setting state.

---
+++ app/src/main/java/com/example/android/popularmovies/model/PopularMoviesSettings.java
@@ -0,0 +1,24 @@
+package com.example.android.popularmovies.model;
+
+public class PopularMoviesSettings
+{
+  public static final int TOP_RATED = 0;
+  public static final int MOST_POPULAR = 1;
+
+  private int sortSetting;
+
+  public PopularMoviesSettings(int sortSetting)
+  {
+    this.sortSetting = sortSetting;
+  }
+
+  public int getSortSetting()
+  {
+    return this.sortSetting;
+  }
+
+  public void setSortSetting(int sortSetting)
+  {
+    this.sortSetting = sortSetting;
+  }
+}
