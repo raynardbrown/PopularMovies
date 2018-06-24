f2aa58fcc0eb90521a7563b3630a98280b16daa2
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-06-24 03:52:59 -0500

    Updated to support pulling the raw movie poster image data from the favorites database.

--- app/src/main/java/com/example/android/popularmovies/MoviePosterImageAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/MoviePosterImageAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.popularmovies;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +10,11 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import com.example.android.popularmovies.db.MovieFavoriteContract;
 import com.example.android.popularmovies.model.MovieListResultObject;
+import com.example.android.popularmovies.tasks.IAsyncTaskCompleteListener;
+import com.example.android.popularmovies.tasks.MovieFavoriteDbDeleteAsyncTask;
+import com.example.android.popularmovies.tasks.MovieFavoriteDbQueryAsyncTask;
 import com.example.android.popularmovies.utils.TheMovieDatabaseUtils;
 import com.squareup.picasso.Picasso;
 
@@ -62,13 +68,63 @@ public class MoviePosterImageAdapter extends BaseAdapter
       imageView = (ImageView) convertView;
     }
 
-    Uri uri = TheMovieDatabaseUtils.getMoviePosterUriFromPath(context, movieListResultObjectList.get(position).getPosterPath());
-
-    Picasso.with(this.context)
-            .load(uri.toString())
-            .placeholder(android.R.drawable.star_big_on) // TODO: Use a real loading image :-)
-            .into(imageView);
+    DbQueryPojo dbQueryPojo = new DbQueryPojo(imageView, movieListResultObjectList.get(position).getPosterPath());
+    MovieFavoriteDbQueryAsyncTask.Param param = new MovieFavoriteDbQueryAsyncTask.Param(movieListResultObjectList.get(position).getId(), dbQueryPojo);
+    new MovieFavoriteDbQueryAsyncTask(this.context, new MovieFavoriteDbQueryAsyncTaskCompleteListener()).execute(param);
 
     return imageView;
+  }
+
+  private class DbQueryPojo
+  {
+    ImageView imageView;
+    String moviePosterPath;
+
+    DbQueryPojo(ImageView imageView, String moviePosterPath)
+    {
+      this.imageView = imageView;
+      this.moviePosterPath = moviePosterPath;
+    }
+  }
+
+  class MovieFavoriteDbQueryAsyncTaskCompleteListener implements IAsyncTaskCompleteListener<MovieFavoriteDbQueryAsyncTask.Result>
+  {
+    @Override
+    public void onTaskComplete(MovieFavoriteDbQueryAsyncTask.Result result)
+    {
+      if(result != null)
+      {
+        Cursor cursor = result.cursor;
+
+        if(cursor != null)
+        {
+          if(cursor.moveToFirst())
+          {
+            // use the image data from the favorites database
+            byte[] moviePosterImageData = cursor.getBlob(cursor.getColumnIndex(MovieFavoriteContract.MovieFavorites.MOVIE_POSTER_IMAGE_DATA));
+
+            ImageView imageView = ((DbQueryPojo)result.extraData).imageView;
+            imageView.setImageBitmap(BitmapFactory.decodeByteArray(moviePosterImageData, 0, moviePosterImageData.length));
+          }
+          else
+          {
+            // cursor is empty, so fetch the image remotely
+
+            ImageView imageView = ((DbQueryPojo)result.extraData).imageView;
+            String moviePosterPath = ((DbQueryPojo)result.extraData).moviePosterPath;
+
+            Uri uri = TheMovieDatabaseUtils.getMoviePosterUriFromPath(MoviePosterImageAdapter.this.context, moviePosterPath);
+
+            Picasso.with(MoviePosterImageAdapter.this.context)
+                    .load(uri.toString())
+                    .placeholder(android.R.drawable.star_big_on) // TODO: Use a real loading image :-)
+                    .into(imageView);
+          }
+
+          // Done with the cursor so close it
+          cursor.close();
+        }
+      }
+    }
   }
 }
