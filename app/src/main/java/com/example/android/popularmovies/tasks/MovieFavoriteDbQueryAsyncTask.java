7ebdc860a559d05c9f958f44b22e5264e47d6b3b
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-06-24 03:32:58 -0500

    Created the MovieFavoriteDbQueryAsyncTask class that performs movie favorite database queries on a background thread.
    Fixed a bug in the background function that was caused by not building
    up the content uri for the single item case.
    
    Fixed a bug in the doInBackground function caused by checking the params
    array for null instead of checking the array length.
    
    Updated to support a generic extra data parameter.
    
    The movieId field is optional so it was changed to an object which is
    easier differentiate, i.e. null vs. an invalid integer value.

---
+++ app/src/main/java/com/example/android/popularmovies/tasks/MovieFavoriteDbQueryAsyncTask.java
@@ -0,0 +1,120 @@
+package com.example.android.popularmovies.tasks;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import com.example.android.popularmovies.db.MovieFavoriteContract;
+
+import java.lang.ref.WeakReference;
+
+public class MovieFavoriteDbQueryAsyncTask extends AsyncTask<MovieFavoriteDbQueryAsyncTask.Param, Void, MovieFavoriteDbQueryAsyncTask.Result>
+{
+  private WeakReference<Context> context;
+  private IAsyncTaskCompleteListener<Result> listener;
+
+  public static class Param
+  {
+    /**
+     * The id of the movie to query.
+     */
+    private Integer movieId;
+
+    private Object extraData;
+
+    /**
+     * Parameter object for the MovieFavoriteDbQueryAsyncTask class. If the specified movieId is
+     * null then all of the rows in the favorite database are returned. Otherwise only row with the
+     * specified movieId are returned.
+     *
+     * @param movieId all rows specified by this id.
+     *
+     * @param extraData extra data that is passed along to the results object.
+     */
+    public Param(Integer movieId, Object extraData)
+    {
+      this.movieId = movieId;
+      this.extraData = extraData;
+    }
+  }
+
+  public class Result
+  {
+    public Cursor cursor;
+
+    public Object extraData;
+
+    Result(Cursor cursor, Object extraData)
+    {
+      this.cursor = cursor;
+      this.extraData = extraData;
+    }
+  }
+
+  public MovieFavoriteDbQueryAsyncTask(Context context, IAsyncTaskCompleteListener<Result> listener)
+  {
+    this.context = new WeakReference<Context>(context);
+    this.listener = listener;
+  }
+
+  @Override
+  protected Result doInBackground(Param... params)
+  {
+    try
+    {
+      Context weakContext = context.get();
+
+      if(weakContext != null)
+      {
+        if(params.length > 0)
+        {
+          if(params[0].movieId != null)
+          {
+            // query one item
+
+            // Make sure to use the correct URI
+            Uri uri = MovieFavoriteContract.MovieFavorites.CONTENT_URI;
+            uri = uri.buildUpon().appendPath(Integer.toString(params[0].movieId)).build();
+
+            return new Result(weakContext.getContentResolver().query(uri,
+                    null,
+                    null,
+                    null,
+                    null), params[0].extraData);
+          }
+          else
+          {
+            // get all rows
+            return new Result(weakContext.getContentResolver().query(MovieFavoriteContract.MovieFavorites.CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    null), params[0].extraData);
+          }
+        }
+        else
+        {
+          // should throw exception since params are required
+          return null;
+        }
+      }
+      else
+      {
+        return null;
+      }
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  @Override
+  protected void onPostExecute(Result result)
+  {
+    super.onPostExecute(result);
+    listener.onTaskComplete(result);
+  }
+}
