e60cd4a3b874eb3203121fcaf41540e1df8c6b3b
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-04-15 23:46:45 -0500

    docs: Document the onTaskComplete function

--- app/src/main/java/com/example/android/popularmovies/tasks/IAsyncTaskCompleteListener.java
+++ app/src/main/java/com/example/android/popularmovies/tasks/IAsyncTaskCompleteListener.java
@@ -2,5 +2,11 @@ package com.example.android.popularmovies.tasks;
 
 public interface IAsyncTaskCompleteListener<T>
 {
+  /**
+   * Called by the async task where this IAsyncTaskCompleteListener is registered. This called is
+   * executed on the UI thread.
+   *
+   * @param result the result of the return by the async task after its execution has completed.
+   */
   public void onTaskComplete(T result);
 }
